1a0d0fab98ad1e15c488d6a06d915b0cea10a42a
JoseSosaAzua <josseazua@gmail.com>
2023-12-15 12:46:53 -0600

    FEAT: main layout added to UI

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,45 @@
 <script>
-  
-	import NavBar from './components/NavBar.svelte';
-  import "../app.pcss";
+import NavBar from './components/NavBar.svelte';
+import "../app.pcss";
 
-  let items = [
-    {tag: "Inicio", link: "/home"},
-    {tag: "Letras", link: "/lyrics"},
-    {tag: "Karaoke", link: "/karaoke"},
-    {tag: "Mas", link: "/more"},
-  ];
-
+let items = [{
+        tag: "Inicio",
+        link: "/home"
+    },
+    {
+        tag: "Letras",
+        link: "/lyrics"
+    },
+    {
+        tag: "Karaoke",
+        link: "/karaoke"
+    },
+    {
+        tag: "Mas",
+        link: "/more"
+    },
+];
 </script>
 
-<NavBar items={items}/>
+<div class="flex flex-col h-screen">
+  <!-- Encabezado -->
+  <header class="bg-blue-500 text-white py-4">
+    <NavBar {items} />
+  </header>
 
-<slot />
+  <!-- Sección central dividida en dos partes -->
+  <main class="flex-1 flex">
+    <section class="w-1/2 bg-gray-200 flex justify-center items-center">
+      <h2 class="text-center py-8 text-lg font-semibold">Sección 1</h2>
+    </section>
+    <section class="w-1/2 bg-gray-300 flex justify-center items-center">
+      <h2 class="text-center py-8 text-lg font-semibold">Sección 2</h2>
+    </section>
+  </main>
+
+  <!-- Pie de página -->
+  <footer class="bg-blue-500 text-white py-4">
+    <h3 class="text-center text-sm">Footer</h3>
+  </footer>
+</div>
+
--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,1 +1,54 @@
-<h1>Home</h1>
+<script>
+// @ts-nocheck
+
+let recognizing = false;
+let outputText = '';
+
+const startRecognition = () => {
+    recognizing = true;
+    const recognition = new webkitSpeechRecognition() || new SpeechRecognition();
+    recognition.lang = 'es-ES';
+    recognition.interimResults = true;
+    recognition.continuous = true;
+
+    recognition.onresult = event => {
+        const result = event.results[event.results.length - 1][0].transcript;
+        outputText = result;
+    };
+
+    recognition.onend = () => {
+        recognizing = false;
+    };
+
+    recognition.onerror = event => {
+        console.error('Speech recognition error:', event.error);
+    };
+
+    recognition.onnomatch = () => {
+        console.log('No speech was recognized.');
+    };
+
+    recognition.start();
+};
+</script>
+
+<div class="flex flex-row bg-orange-400 p-10">
+
+    <div class="bg-pink-600 flex items-center justify-center w-full">
+        <button
+            on:click={startRecognition}
+            disabled={recognizing}
+            class=""
+            >
+            {recognizing ? 'Recording...' : 'Start Recording'}
+        </button>
+    </div>
+
+    <div class="bg-slate-900 flex flex-col w-full">
+        <h1 class="text-5xl">Speech to Text</h1>
+        <span>
+            {outputText}
+        </span>
+    </div>
+
+</div>
